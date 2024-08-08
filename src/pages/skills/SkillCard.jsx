5573ac314659db57f475abf90c61a49be23abda0
Alex-LikesPizza <alexandru13321@gmail.com>
2024-08-08 22:02:51 +0300

    more cards

--- src/pages/skills/SkillCard.jsx
+++ src/pages/skills/SkillCard.jsx
@@ -2,21 +2,21 @@ import React from 'react'
 import Progress from '../../components/decorations/Progress'
 
 
-function SkillCard() {
+function SkillCard({title, rank, postRank, xp, src}) {
   return (
     <div className='card'>
       <div className='u-flex u-justify-between'>
         <div>
-          <h2 className='card__title'>JavaScript</h2>
-          <h2 className='card__subtitle'>Beginner</h2>
+          <h2 className='card__title'>{title}</h2>
+          <h2 className='card__subtitle'>{rank}</h2>
         </div>
-        <img width={100} height={100} src="" alt="" />
+        <img className='card__image' width={100} height={100} src={src} alt="" />
       </div>
       <br />
       <Progress 
-        from="Beginner"
-        to="Intermediate"
-        value={40}
+        from={rank}
+        to={postRank}
+        value={xp}
       />
     </div>
   )
--- src/pages/skills/Skills.jsx
+++ src/pages/skills/Skills.jsx
@@ -1,11 +1,43 @@
 import React from 'react'
 import SkillCard from './SkillCard'
 
+import imgJS from "../../assets/js.png";
+import imgSASS from "../../assets/sass.png"
+import imgUX_UI from "../../assets/u.png"
+import imgReact from "../../assets/react.png"
+
 function Skills() {
   return (
     <div>
       <h1>Skills</h1>
-      <SkillCard></SkillCard>
+      <SkillCard
+        title="Javascript"
+        rank="Mid-level"
+        postRank="Senior"
+        xp={36}
+        src={imgJS}
+      ></SkillCard>
+      <SkillCard
+        title="CSS & SASS"
+        rank="Mid-level"
+        postRank="Senior"
+        xp={45}
+        src={imgSASS}
+      ></SkillCard>
+      <SkillCard
+        title="UX / UI"
+        rank="Mid-level"
+        postRank="Senior"
+        xp={30}
+        src={imgUX_UI}
+      ></SkillCard>
+      <SkillCard
+        title="React"
+        rank="Junior"
+        postRank="Mid-level"
+        xp={30}
+        src={imgReact}
+      ></SkillCard>
     </div>
 
   )
