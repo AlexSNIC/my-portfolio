8511e111a72e4b4e7453d65d084312beb4df0ef3
Alex-LikesPizza <alexandru13321@gmail.com>
2024-08-09 22:58:39 +0300

    Skills grid display

--- src/pages/skills/SkillCard.jsx
+++ src/pages/skills/SkillCard.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import Progress from '../../components/decorations/Progress'
 
 
-function SkillCard({title, rank, postRank, xp, src}) {
+function SkillCard({title, rank, postRank, xp, src, list}) {
   return (
     <div className='card'>
       <div className='u-flex u-justify-between'>
         <div>
           <h2 className='card__title'>{title}</h2>
           <h2 className='card__subtitle'>{rank}</h2>
+          <ul className='card__list'>
+            {list?.map((item, index) => {
+              return <li key={index}>{item}</li>
+            })}
+          </ul>
         </div>
         <img className='card__image' width={100} height={100} src={src} alt="" />
       </div>
--- src/pages/skills/Skills.jsx
+++ src/pages/skills/Skills.jsx
@@ -2,34 +2,49 @@ import React from 'react'
 import SkillCard from './SkillCard'
 
 import imgJS from "../../assets/js.png";
-import imgSASS from "../../assets/sass.png"
-import imgUX_UI from "../../assets/u.png"
+import imgStyles from "../../assets/styles.png"
+import imgUX_UI from "../../assets/ux.png"
 import imgReact from "../../assets/react.png"
+import imgCpp from "../../assets/cpp.png"
+import imgFirebase from "../../assets/firebase.png"
 
 function Skills() {
   return (
-    <div>
-      <h1>Skills</h1>
+  <div className="skills">
+    <div className='skills__wrapper'>
       <SkillCard
-        title="Javascript"
+        title="JS & TS"
         rank="Mid-level"
         postRank="Senior"
         xp={36}
         src={imgJS}
-      ></SkillCard>
+        list={[
+          "250+ hours",
+          "*JQuery",
+          ]}
+        ></SkillCard>
       <SkillCard
-        title="CSS & SASS"
+        title="Styles"
         rank="Mid-level"
         postRank="Senior"
-        xp={45}
-        src={imgSASS}
+        xp={5}
+        src={imgStyles}
+        list={[
+          "160+ hours",
+          "CSS & SASS",
+          "*PostCSS",
+          "*Tailwind",
+        ]}
       ></SkillCard>
       <SkillCard
         title="UX / UI"
-        rank="Mid-level"
-        postRank="Senior"
+        rank="Junior"
+        postRank="Mid-level"
         xp={30}
         src={imgUX_UI}
+        list={[
+          "*Figma"
+        ]}
       ></SkillCard>
       <SkillCard
         title="React"
@@ -37,8 +52,36 @@ function Skills() {
         postRank="Mid-level"
         xp={30}
         src={imgReact}
+        list={[
+          "50+ hours",
+          "JSX & *TSX"
+        ]}
+      ></SkillCard>
+      <SkillCard
+        title="Firebase"
+        rank="Trainee"
+        postRank="Junior"
+        xp={50}
+        src={imgFirebase}
+        list={[
+          "Firestore",
+          "Realtime Database",
+          "Authentication"
+        ]}
+      ></SkillCard>
+      <SkillCard
+        title="C++"
+        rank="Junior"
+        postRank="Mid-level"
+        xp={60}
+        src={imgCpp}
+        list={[
+          "150+ hours",
+          "Algorithms & Data Structures"
+        ]}
       ></SkillCard>
     </div>
+  </div>
 
   )
 }
