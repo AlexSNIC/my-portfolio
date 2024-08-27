df8624b54f914a53383588783e85b1df2e800909
Solomon Alexandru - Nicolae <alexandru13321@gmail.com>
2024-08-27 22:11:13 +0300

    .

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import Button from '../../components/button/Button';
 import Type from '../../components/decorations/Type';
 import portrait from  "../../assets/portrait-min.jpg";
